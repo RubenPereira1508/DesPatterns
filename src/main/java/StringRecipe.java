1dc4e3e84e3db908d40f82fbeaaeae245d5616e8
Ruben <up202006195@fe.up>
2022-11-16 17:44:57 +0000

    Point 1 all done

--- src/main/java/StringRecipe.java
+++ src/main/java/StringRecipe.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringRecipe implements StringTransformer{
+
+    List<StringTransformer> transformers1 = new ArrayList<>();
+
+
+    StringRecipe(List<StringTransformer> transformers){
+    transformers1 = transformers;
+
+    }
+
+    public void mix(StringDrink drink) {
+        for (StringTransformer stringTransformer : transformers1) {
+            stringTransformer.execute(drink);
+        }
+    }
+
     @Override
     public void execute(StringDrink drink) {
-
     }
 }
